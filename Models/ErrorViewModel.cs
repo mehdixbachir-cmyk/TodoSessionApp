e0b171cf65d1d638532f9bfd11979d8fc8412956
mehdixbachir-cmyk <mehdixbachir@gmail.com>
2025-12-14 17:06:46 +0100

    Initial commit - Todo MVC Session

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TodoSessionApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
